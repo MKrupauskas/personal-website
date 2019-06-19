e65545e1851c2e27c37ea6998d0a57466ba155bd
Mykolas Krupauskas <mkrupauskas@gmail.com>
2019-06-19 16:35:56 +0300

    added log rocket

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,7 @@ import 'typeface-merriweather'
 
 // prismjs syntax highlighting
 import 'prismjs/themes/prism-solarizedlight.css'
+
+import LogRocket from 'logrocket';
+
+LogRocket.init('uj9osr/mkrupcom');
