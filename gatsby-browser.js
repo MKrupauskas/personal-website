3e076b261dad47dd8eff92b68b081390b3ce1ddc
Mykolas Krupauskas <mkrupauskas@gmail.com>
2020-05-24 13:21:36 +0200

    add logging only in production

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,10 @@ import 'typeface-merriweather'
 import 'prismjs/themes/prism-solarizedlight.css'
 
 // error logging
-import * as Sentry from '@sentry/browser';
+import * as Sentry from '@sentry/browser'
 
-Sentry.init({ dsn: "https://6b377ceb129d49e789903556b0808cbe@sentry.io/1486761" });
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://6b377ceb129d49e789903556b0808cbe@sentry.io/1486761',
+  })
+}
