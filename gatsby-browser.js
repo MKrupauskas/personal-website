914e02fda6efa274f5d308d47488cc243cd032d5
Mykolas Krupauskas <mkrupauskas@gmail.com>
2021-09-25 22:46:05 +0300

    add dark syntax theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,6 @@
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-// prismjs syntax highlighting
-import 'prismjs/themes/prism-solarizedlight.css'
-
 // error logging
 import * as Sentry from '@sentry/browser'
 
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,7 @@ import Typography from 'typography'
 import Wordpress2016 from 'typography-theme-wordpress-2016'
 
 import './global.css'
+import './syntax.css'
 
 Wordpress2016.overrideThemeStyles = () => {
   return {
